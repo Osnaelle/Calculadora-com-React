aee1d114e52a59cbe97c297f2337f31aeaeba603
Osnaelle <naellyfarias@gmail.com>
2022-11-21 20:54:19 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculadora from './main/Calculadora';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+<div>Calculadora</div>
+
+root.render(
+    
+     <Calculadora/>
+      
+    
+  
+
+);
+
